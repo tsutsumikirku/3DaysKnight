7e265f26f7d67a594ebcaf05c2c22d6329747b47
mikenchi <mikenti337564@gmail.com>
2024-06-10 19:09:18 +0900

    エラー箇所を改善しました

--- Assets/script/nextLv.cs
+++ Assets/script/nextLv.cs
@@ -5,21 +5,17 @@ using UnityEngine.UI;
 
 public class nextLv : MonoBehaviour
 {
-    Text text;
-    [SerializeField] GameObject mainplayer = null;
-    [SerializeField] int next = 100;
-    int nextlv;
+
     // Start is called before the first frame update
     void Start()
     {
         
-        text = GetComponent<Text>();
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-       nextlv =       
-      text.text = nextlv.ToString();
+     
     }
 }
