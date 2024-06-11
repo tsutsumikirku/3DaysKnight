79bba88f7799d26153ee64d4a9b827600449e196
mikenchi <mikenti337564@gmail.com>
2024-06-11 18:17:44 +0900

    [add]HPと次のレベルを表示するスクリプトを作りました

--- Assets/script/mainPlayer.cs
+++ Assets/script/mainPlayer.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mainPlayer : MonoBehaviour
 {
+    Text text;
+    [SerializeField] float Hp = 5f;
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpPower = 5f;
-    public int level = 1;
+    [SerializeField] GameObject nextlv;
+    [SerializeField] GameObject Hptex;
+    
+    public float level = 1f;
     Rigidbody2D rb;
    public int bazzarl = 0;
     float move;
     int weapon = 1;
+    float nextlev;
+    
     // Start is called before the first frame update
     void Start()
     {
+       
+
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Text Hplih = Hptex.GetComponent<Text>();
+        Hplih.text ="HP" +  Hp.ToString();
+
+
+
+
+        nextlev = level * 100;
+        Text nextlevel = nextlv.GetComponent<Text>();
+        nextlevel.text ="NextLv" + nextlev.ToString();
+        
         move = Input.GetAxis("Horizontal");
         rb.AddForce(Vector2.right * move * moveSpeed);
 
