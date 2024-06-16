4acd40b9f19435fbe9b81b839c1f3756d96a00f5
mikenchi <mikenti337564@gmail.com>
2024-06-16 21:14:47 +0900

    [add]プルし忘れデータが吹っ飛んだのでその修正を始めました

--- Assets/script/mainPlayer.cs
+++ Assets/script/mainPlayer.cs
@@ -5,24 +5,19 @@ using UnityEngine.UI;
 
 public class mainPlayer : MonoBehaviour
 {
-    Text text;
-    [SerializeField] float Hp = 5f;
+ 
+    
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpPower = 5f;
-<<<<<<< HEAD
-    public Text nextLevel;
+     public Text nextLevel;
     public int level = 1;
     public int bazzarl = 0;
-=======
-    [SerializeField] GameObject nextlv;
-    [SerializeField] GameObject Hptex;
+ 
     
-    public float level = 1f;
->>>>>>> 79bba88f7799d26153ee64d4a9b827600449e196
+   
     Rigidbody2D rb;
     float move;
     int weapon = 1;
-    float nextlev;
     
     // Start is called before the first frame update
     void Start()
@@ -36,19 +31,9 @@ public class mainPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Text Hplih = Hptex.GetComponent<Text>();
-        Hplih.text ="HP" +  Hp.ToString();
-
-
-
-
-        nextlev = level * 100;
-        Text nextlevel = nextlv.GetComponent<Text>();
-        nextlevel.text ="NextLv" + nextlev.ToString();
-        
         move = Input.GetAxis("Horizontal");
         rb.AddForce(Vector2.right * move * moveSpeed);
-        if(move == 0)
+        if(move < 0.5 || move > -0.5)
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
