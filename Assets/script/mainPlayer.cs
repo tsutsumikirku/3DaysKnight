0b3bda12d9f4091e5288ca646add374592d1cf63
mikenchi <mikenti337564@gmail.com>
2024-06-17 19:54:27 +0900

    [add]ジャンプのインターバルを設定しました

--- Assets/script/mainPlayer.cs
+++ Assets/script/mainPlayer.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 
 public class mainPlayer : MonoBehaviour
 {
-    
+
     [SerializeField] float hp = 5f;
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpPower = 5f;
+    [SerializeField] float jumpin;
     public int level = 1;
     public int bazzarl = 0;
     public Text hptex;
-     
+   
+    float jumpti;
+
 
 
 
@@ -21,12 +24,15 @@ public class mainPlayer : MonoBehaviour
     Rigidbody2D rb;
     float move;
     int weapon = 1;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        //ジャンプのインターバルのプログラム
+        jumpti = jumpin;
 
-    
+     
+
 
 
 
@@ -35,33 +41,34 @@ public class mainPlayer : MonoBehaviour
 
         //ゲットコンポーネント
         rb = GetComponent<Rigidbody2D>();
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        jumpti += Time.deltaTime;
         //テキストボックスの表示プログラム
         hptex.text = "HP" + hp;
-        
+
         //移動のプログラム
         move = Input.GetAxis("Horizontal");
 
         rb.AddForce(Vector2.right * move * moveSpeed);
 
 
-        if(move < 0.5 || move > -0.5)
+        if (move < 0.5 || move > -0.5)
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        
+
         //バザールと武器管理のプログラム予定
-        if(weapon == 1 && bazzarl == 0)
+        if (weapon == 1 && bazzarl == 0)
         {
 
         }
-        if(weapon == 2 && bazzarl == 0)
+        if (weapon == 2 && bazzarl == 0)
         {
 
         }
@@ -76,10 +83,11 @@ public class mainPlayer : MonoBehaviour
     //ジャンプ処理
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "floor")
+        if (collision.collider.tag == "floor" )
         {
-            if(Input.GetButton("Jump"))
+            if(Input.GetButton("Jump") && jumpin < jumpti )
             {
+                jumpti = 0;
                 rb.AddForce(Vector2.up * jumpPower * 3);
             }
         }
